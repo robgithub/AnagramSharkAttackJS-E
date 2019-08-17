1c2ede6873bf700a29a9b65ff81aa38dc2ecb2e1
robgithub <rob.github@jumpstation.co.uk>
2019-08-17 21:39:04 +0100

    game todo and word list

--- TODO.md
+++ TODO.md
@@ -17,15 +17,35 @@ Assets/components:
   Letter Tile template [-] (basic and hardcoded)
   Clouds [X]
   Title [ ]
-  Sonar [-]
-  Radar sharks [-]
-  Sharks [-]
+  Sonar [X]
+  Radar sharks [X]
+  Sharks [X]
   Game Over mouth [ ]
   Word lists [ ]
   Instructions [ ]
      
 Create GitHub project .io page [X]
 https://robgithub.github.io/AnagramSharkAttackJS-E/
+
+
+Game play:
+  Start button [ ]
+  Word selection per level [ ]
+  calculate letter positions [ ]
+  animate titles to start locations [ ]
+  tiles draggable [ ]
+  tile release -> closest column [ ]
+  tile bumping [ ]
+  timer for sharks [ ]
+  animation for blips [ ]
+  shark tile attack [ ]
+  tile return [ ]
+  completion [ ]
+  death [ ]
+  timer [ ]
+  new level [ ]
+  level indicator [ ]
+
 
 
 Post launch ideas.
--- game.js
+++ game.js
@@ -6,28 +6,30 @@ Anagram Shark Attack JS-E
 
 */
 
+
 function game(name, targetDiv) {
   var game = this; 
   game.targetDiv = targetDiv; 
   var debugLevel = 0;
   var tiles = [];
+  var words = [ ['fish','boat','ship','crab','gull'], ['','','','','' ], ['','','','','' ], ['','','','','' ], ['','','','','' ], ['','','','','' ] ];
 
   game.start = function() {
     game.debug(0, "it has begun");
-    tiles.push(new tile());
+    tiles.push(new tile('S'));
     document.querySelector(game.targetDiv).append(tiles[0].create());
   }
 
 
-	// return random value between 0 and 1.0
-	game.getRandom = function() {
-		return Math.random();
-	};
+  // return random value between 0 and 1.0
+  game.getRandom = function() {
+    return Math.random();
+  };
 
-	// return random float value between min and max (including min and up to but not including the whole number max)
-	game.getRandomRange = function(min,max) {
-		return min + ( max * this.getRandom());
-	};
+  // return random float value between min and max (including min and up to but not including the whole number max)
+  game.getRandomRange = function(min,max) {
+    return min + ( max * this.getRandom());
+  };
 
   // log any debug messages that are at the threshold of debugLevel or below.
   game.debug  = function(debugThreshold, message) {
@@ -35,17 +37,19 @@ function game(name, targetDiv) {
       console.log(message);
     }
   }
+
 }
 
-function tile() {
+function tile(letter) {
   var tile = this;
+  tile.letter = letter;
   
   tile.create = function() {
     var element = document.createElement('div');
     element.className = "tile";
     var innerElement = document.createElement('div');
     innerElement.className = "inner";
-    innerElement.innerHTML = '<svg viewBox="0 0 100 100"><text x="18" y="90%">X</text></svg>';
+    innerElement.innerHTML = '<svg viewBox="0 0 100 100"><text x="18" y="90%">'+ tile.letter +'</text></svg>';
     element.append(innerElement);
     return element;
   };
