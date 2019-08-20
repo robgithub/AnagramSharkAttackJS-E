59e91488b0a31adea71084f57ce9f6eb47792822
robgithub <rob.github@jumpstation.co.uk>
2019-08-20 08:53:16 +0100

    word list v.1

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ function game(name, targetDiv) {
   game.targetDiv = targetDiv; 
   var debugLevel = 0;
   var tiles = [];
-  var words = [ ['fish','boat','ship','crab','tuna'], ['ocean','whale','shark','waves','shrimp' ], ['lobster','dolphin','octopus','seaweed','penguin' ], ['barnacle','seasnake','morayeel','mantaray','flyingfish' ], ['jellyfish','clownfish','bluewhale','swordfish','bobitworm' ], ['pufferfish','seacucumber','nudibrank','nourtilus','giantclam' ] ];
+  var words = [ ['fish','boat','ship','crab','tuna'], ['ocean','whale','shark','waves','shrimp' ], ['lobster','dolphin','octopus','seaweed','penguin' ], ['barnacle','seasnake','morayeel','mantaray','flyingfish' ], ['jellyfish','clownfish','bluewhale','swordfish','nautilus' ], ['pufferfish','seacucumber','nudibranch','bobbitworm','giantclam' ] ];
 
   game.start = function() {
     game.debug(0, "it has begun");
